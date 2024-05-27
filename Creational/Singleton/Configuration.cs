4879f171943fecd2431a6bb20ea3b28734fabafb
Shonsu <shonsu82@gmail.com>
2024-05-27 19:28:51 +0200

    Singleton

---
+++ Creational/Singleton/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton;
+
+public class Configuration
+{
+    private static Configuration _instance = null;
+    private static readonly object _lock = new object();
+    public string StringProperty { get; set; }
+    public int IntProperty { get; set; }
+
+    private Configuration() { }
+
+    public static Configuration GetInstance()
+    {
+        lock (_lock)
+        {
+            if (_instance == null)
+            {
+                _instance = new Configuration();
+            }
+        }
+        return _instance;
+    }
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+
+var cfg = Configuration.GetInstance();
+var cfg1 = Configuration.GetInstance();
+
+System.Console.WriteLine("Thisa singleton: " + ReferenceEquals(cfg, cfg1));
