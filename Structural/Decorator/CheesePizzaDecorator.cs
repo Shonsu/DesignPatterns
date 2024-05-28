26ffb60ef05706c1052f1eb4d23bc46da92aaed9
Shonsu <shonsu82@gmail.com>
2024-05-28 13:45:40 +0200

    Decorator

---
+++ Structural/Decorator/CheesePizzaDecorator.cs
@@ -0,0 +1,13 @@
+﻿namespace Decorator;
+
+public class CheesePizzaDecorator : PizzaDecorator
+{
+    public CheesePizzaDecorator(IPizza pizza) : base(pizza)
+    {
+    }
+
+    public override double CalculatePrice()
+    {
+        return base.CalculatePrice() + 4.5;
+    }
+}
---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Decorator;
+
+var pizzaBase = new MediumPizza();
+
+var mediumPizzaWithCheese = new CheesePizzaDecorator(pizzaBase);
+System.Console.WriteLine($"mediumPizzaWithCheese price: {mediumPizzaWithCheese.CalculatePrice()}");
+
+var mediumPizzaWithCheeseHam = new HamPizzaDecorator(mediumPizzaWithCheese);
+
+System.Console.WriteLine($"mediumPizzaWithCheeseHam price: {mediumPizzaWithCheeseHam.CalculatePrice()}");
+
+var mediumPizzaWithCheese2Ham = new HamPizzaDecorator(mediumPizzaWithCheeseHam);
+
+System.Console.WriteLine($"mediumPizzaWithCheeseDoubleHam price: {mediumPizzaWithCheese2Ham.CalculatePrice()}");
